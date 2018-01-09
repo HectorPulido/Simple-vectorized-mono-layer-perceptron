d7296b931d97491eb068fbd6323c2bdb5394754d
Hector Pulido <Hectorandrespp@gmail.com>
2018-01-08 22:04:19 -0500

    First commit

--- SimpleNeuralNetwork/SimpleNeuralNetwork/Program.cs
+++ SimpleNeuralNetwork/SimpleNeuralNetwork/Program.cs
@@ -11,68 +11,65 @@ namespace SimpleNeuralNetwork
     {
         static void Main(string[] args)
         {
-            while (true)
+            //Parameters
+            int inputCount = 2;
+            int hiddenCount = 5;
+            int outputCount = 4;
+            int examplesCount = 4;
+            double learningRate = 0.2;
+            Random r = new Random();
+
+            //Training data           // INPUT
+            Matrix x = new double[,] { { 0, 0}, 
+                                        { 0, 1}, 
+                                        { 1, 0}, 
+                                        { 1, 1} };  
+                                        //DESIRED OUTPUT
+                                    //XNOR AND OR XOR
+            Matrix y = new double[,] { { 1, 0, 0, 0 }, 
+                                        { 0, 0, 1, 1 }, 
+                                        { 0, 1, 1, 1 }, 
+                                        { 1, 1, 1, 0 } }; 
+
+            //Weight init
+            Matrix w1 = (Matrix.Random(inputCount + 1, hiddenCount , r) - 0.5) * 2.0;
+            Matrix w2 = (Matrix.Random(hiddenCount + 1, outputCount, r) - 0.5) * 2.0;
+
+            for (int l = 0; l < 5001; l++) //epoch
             {
-                //Parameters
-                int inputCount = 2;
-                int hiddenCount = 5;
-                int outputCount = 4;
-                int examplesCount = 4;
-                double learningRate = 0.2;
-                Random r = new Random();
+                //Forward pass
+                Matrix z1 = x.AddColumn(Matrix.Ones(examplesCount, 1));
+                Matrix a1 = z1; //(Examples, input + 1)
+                Matrix z2 = (a1 * w1).AddColumn(Matrix.Ones(examplesCount, 1));
+                Matrix a2 = sigmoid(z2); //(examples, hidden neurons + 1) // APPLY NON LINEAR
+                Matrix z3 = a2 * w2;
+                Matrix a3 = sigmoid(z3); //(examples, output)                  
 
-                //Training data           // INPUT
-                Matrix x = new double[,] { { 0, 0}, 
-                                           { 0, 1}, 
-                                           { 1, 0}, 
-                                           { 1, 1} };  
-                                         //DESIRED OUTPUT
-                                        //XNOR AND OR XOR
-                Matrix y = new double[,] { { 1, 0, 0, 0 }, 
-                                           { 0, 0, 1, 1 }, 
-                                           { 0, 1, 1, 1 }, 
-                                           { 1, 1, 1, 0 } }; 
+                //Bacpropagation
+                Matrix a3Error = a3 - y; //(examples, output) //LOSS 
+                Matrix Delta3 = a3Error * sigmoid(z3, true);
 
-                //Weight init
-                Matrix w1 = (Matrix.Random(inputCount + 1, hiddenCount , r) - 0.5) * 2.0;
-                Matrix w2 = (Matrix.Random(hiddenCount + 1, outputCount, r) - 0.5) * 2.0;
+                Matrix a2Error = Delta3 * w2.T;
+                Matrix Delta2 = a2Error * sigmoid(z2, true);
+                Delta2 = Delta2.Slice(0, 1, Delta2.x, Delta2.y); //Slicing Extra delta (from biass neuron)
 
-                for (int l = 0; l < 5001; l++) //epoch
+                w2 -= (a2.T * Delta3) * learningRate;
+                w1 -= (a1.T * Delta2) * learningRate;
+
+                double loss = a3Error.abs.average;
+                Console.WriteLine("Loss: " + loss);
+
+                if (l % 1000 == 0)
                 {
-                    //Forward pass
-                    Matrix z1 = x.AddColumn(Matrix.Ones(examplesCount, 1));
-                    Matrix a1 = z1; //(Examples, input + 1)
-                    Matrix z2 = (a1 * w1).AddColumn(Matrix.Ones(examplesCount, 1));
-                    Matrix a2 = sigmoid(z2); //(examples, hidden neurons + 1) // APPLY NON LINEAR
-                    Matrix z3 = a2 * w2;
-                    Matrix a3 = sigmoid(z3); //(examples, output)                  
+                    Console.WriteLine("---------"+l+"----------------------------------------------------");
+                    Console.WriteLine("X: " + x.size.ToString());
+                    Console.WriteLine(x.ToString());
 
-                    //Bacpropagation
-                    Matrix a3Error = a3 - y; //(examples, output) //LOSS 
-                    Matrix Delta3 = a3Error * sigmoid(z3, true);
-
-                    Matrix a2Error = Delta3 * w2.T;
-                    Matrix Delta2 = a2Error * sigmoid(z2, true);
-                    Delta2 = Delta2.Slice(0, 1, Delta2.x, Delta2.y); //Slicing Extra delta (from biass neuron)
-
-                    w2 -= (a2.T * Delta3) * learningRate;
-                    w1 -= (a1.T * Delta2) * learningRate;
-
-                    double loss = a3Error.abs.average;
-                    Console.WriteLine("Loss: " + loss);
-
-                    if (l % 1000 == 0)
-                    {
-                        Console.WriteLine("---------"+l+"----------------------------------------------------");
-                        Console.WriteLine("X: " + x.size.ToString());
-                        Console.WriteLine(x.ToString());
-
-                        Console.WriteLine("Prediction: " + a3.size.ToString());
-                        Console.WriteLine(a3);                                                
-                    }
+                    Console.WriteLine("Prediction: " + a3.size.ToString());
+                    Console.WriteLine(a3);                                                
                 }
-                Console.ReadKey();
             }
+            Console.ReadKey();            
         }
 
         static Matrix sigmoid(Matrix m, bool derivated = false)
@@ -94,21 +91,5 @@ namespace SimpleNeuralNetwork
 
             return a;
         } 
-        static Matrix relu(Matrix m, bool derivated = false)
-        {
-            double[,] a = m;
-            Matrix.MatrixLoop((i, j) =>
-            {
-                if (derivated)
-                {
-                    a[i, j] = (a[i, j] > 0) ? 1 : 0.001;
-                }
-                else
-                {
-                    a[i, j] = (a[i, j] > 0) ? a[i, j] : 0;
-                }
-            }, a.GetLength(0), a.GetLength(1));
-            return a;
-        }
     }
 }
